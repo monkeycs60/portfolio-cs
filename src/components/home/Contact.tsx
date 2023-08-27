af153e320d42a44a94ca2ea4056b560e9bb0f2cd
Clement SERIZAY <clement.serizay@gmail.com>
2023-08-27 05:17:55 +0200

    feat: add a contact page

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,12 +1,75 @@
+import { clsx } from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+	HardDriveDownload,
+	Github,
+	Linkedin,
+	MousePointer,
+} from 'lucide-react';
+
 const Contact = () => {
 	return (
-		<div className='w-screen bg-gray-cool-700'>
-			<div className='max-w-[1200px] m-auto h-screen p-4 flex flex-col pt-[8vh]'>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Dolorum, possimus. Esse architecto repellat distinctio laboriosam
-					placeat doloremque, nobis voluptatem reiciendis?
-				</p>
+		<div className='w-screen bg-gray-cool-700 font-poppins'>
+			<div className='w-[80%]  m-auto py-[15vh] p-4 flex flex-col gap-32'>
+				<div className='flex flex-col gap-2'>
+					<div className='flex lg:h-[150px] items-center gap-12'>
+						<div className='lg:h-[150px] bg-white rounded-2xl'>
+							<Image
+								src='/photo-contact.png'
+								alt='Logo'
+								width={151}
+								height={151}
+								priority={true}
+								className={clsx('lg:w-[150px] lg:h-[150px]')}
+							/>
+						</div>
+						<div className='flex flex-col gap-2'>
+							<p className='text-7xl text-white font-light'>
+								Construisons ensemble
+							</p>
+							<p className='text-7xl text-white  font-light'>
+								Votre projet
+							</p>
+						</div>
+					</div>
+					<div></div>
+				</div>
+				<div className='relative'>
+					<div className='w-full h-[2px] bg-gray-cool-300 '></div>
+					<div className='bg-[#B3CDE0] rounded-xl text-4xl text-gray-cool-800  h-[200px] absolute right-28 -bottom-[100px] flex justify-center items-center '>
+						<p className='p-8'>Contactez-moi</p>
+						<MousePointer className='lg:w-10 lg:h-10 absolute bottom-4 right-1/3 icon-move-pentacle' />
+					</div>
+				</div>
+				<div className='space-y-8'>
+					<div className='flex gap-8 font-light text-4xl text-white'>
+						<Link
+							href='https://www.linkedin.com/in/cl%C3%A9ment-serizay-044911262/'
+							target='_blank'
+							className='flex items-center gap-3'>
+							<Linkedin className={clsx('lg:w-[32px] lg:h-[32px]')} />
+							<span>LinkedIn</span>
+						</Link>
+						<Link
+							href='/docs/CV_Clement_SERIZAY_summer-2023_v4.pdf'
+							target='_blank'
+							className='flex items-center gap-3'>
+							<HardDriveDownload
+								className={clsx('lg:w-[32px] lg:h-[32px]')}
+							/>
+							<span>CV</span>
+						</Link>
+					</div>
+					<div className=' font-light text-4xl text-white'>
+						<Link
+							href='https://github.com/monkeycs60'
+							className='flex items-center gap-3'>
+							<Github className={clsx('lg:w-[32px] lg:h-[32px]')} />
+							<span>GitHub</span>
+						</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,18 +1,42 @@
 'use client';
 
 import Image from 'next/image';
-import { GitMerge } from 'lucide-react';
+import { GitMerge, GitCompare } from 'lucide-react';
 import clsx from 'clsx';
 import { Tooltip } from 'react-tooltip';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const Hero = () => {
+	const [isHover, setIsHover] = useState(false);
 	return (
 		<div className='w-full h-screen p-4 flex flex-col pt-[20vh] '>
 			<div className='flex w-[80vw] h-[35vh] justify-between items-center mx-auto relative'>
 				<div className='h-1/2 flex flex-col justify-center items-center w-1/2'>
 					<div className='text-6xl font-bold font-poppins text-gray-cool-700 flex flex-col gap-4'>
 						<div className='flex gap-4'>
-							<GitMerge className=' text-yellow-500 lg:w-14 lg:h-14' />
+							{!isHover ? (
+								<Link
+									href='https://github.com/monkeycs60'
+									target='_blank'
+									onMouseEnter={() => setIsHover(true)}
+									onMouseLeave={() => setIsHover(false)}>
+									<GitMerge className=' text-yellow-500 lg:w-14 lg:h-14  transition-all duration-300' />
+								</Link>
+							) : (
+								<Link
+									href='https://github.com/monkeycs60'
+									target='_blank'
+									onMouseLeave={() => setIsHover(false)}>
+									<GitCompare
+										className=' text-yellow-500 lg:w-14 lg:h-14  transition-all duration-300'
+										onMouseEnter={() => setIsHover(true)}
+										onMouseLeave={() => {
+											setIsHover(false);
+										}}
+									/>
+								</Link>
+							)}
 							<div className='flex flex-col gap-2'>
 								<h1>Clément Serizay,</h1>
 								<h2>Développeur Fullstack</h2>
