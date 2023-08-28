e21392d4c71e2a120d9cc4cbb9f5693e3095a7f0
Clement SERIZAY <clement.serizay@gmail.com>
2023-08-28 17:31:07 +0200

    chore: change image for projects

--- src/components/Project/ProjectMockUp.tsx
+++ src/components/Project/ProjectMockUp.tsx
@@ -1,21 +1,54 @@
 import Image from 'next/image';
 
-const ProjectMockUp = () => {
+interface ProjectMockUpProps {
+	containerClassName: string;
+	macSrc: string;
+	macAlt: string;
+	macWidth: number;
+	macHeight: number;
+	macClassName: string;
+	iphoneSrc: string;
+	iphoneAlt: string;
+	iphoneClassName: string;
+}
+
+const ProjectMockUp = ({
+	containerClassName,
+	macSrc,
+	macAlt,
+	macWidth,
+	macHeight,
+	macClassName,
+	iphoneSrc,
+	iphoneAlt,
+	
+	iphoneClassName,
+}: ProjectMockUpProps) => {
 	return (
-		<div className='flex mt-[5vh] relative h-[55vh]  justify-center'>
+		// <div className='flex mt-[15vh] relative h-[55vh]  justify-center'>
+		<div className={containerClassName}>
 			<Image
-				src='/desktop-coworkez.png'
-				alt='my project Chiaroscuro desktop version'
-				width={1092}
-				height={684}
-                className='absolute bottom-0 -left-60'
+				src={macSrc}
+				alt={macAlt}
+				// width={1092}
+				// height={684}
+				width={macWidth}
+				height={macHeight}
+				// className='absolute bottom-0 -left-60'
+				className={macClassName}
+				priority
 			/>
 			<Image
-				src='/mobile-coworkez.png'
-				alt='my project Chiaroscuro mobile version'
+				// src='/mobile-coworkez.png'
+				// alt='my project Chiaroscuro mobile version'
 				width={538}
 				height={1146}
-				className='absolute scale-75 -bottom-52 -right-60'
+				// className='absolute scale-75 -bottom-52 -right-60'
+				src={iphoneSrc}
+				alt={iphoneAlt}
+
+				className={iphoneClassName}
+				priority
 			/>
 		</div>
 	);
