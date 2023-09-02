6d3222ed7d4e9a66a995274dce67cc88410b0db0
Clement SERIZAY <clement.serizay@gmail.com>
2023-09-02 19:06:28 +0200

    style: finish tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -9,67 +9,69 @@ const TechStack = () => {
 					<h3 className='text-xl lg:text-3xl '>Tech Stack</h3>
 					<div className='bg-gray-cool-700 w-[5px] animate-pulse '></div>
 				</div>
-				<div className='flex gap-[200px] justify-between mt-[100px] items-center w-[75%]'>
-					<div className='w-[1000px] h-[100px] justify-center gap-12 flex items-center'>
-						<Image
-							src={'/javascript.svg'}
-							alt='Logo'
-							width={60}
-							height={60}
-							className='w-[72px] h-[72px] '
-						/>
-						<Image
-							src={'/typescript.svg'}
-							alt='Logo'
-							width={60}
-							height={60}
-							className='w-[72px] h-[72px] '
-						/>
+				<div className='flex mt-[100px] h-[350px] w-[70%] bg-gray-100 items-center justify-between text-xl font-poppins p-20'>
+					<div className='flex-col'>
+						<div className='flex gap-10'>
+							<Image
+								src={'/javascript.svg'}
+								alt='Logo'
+								width={60}
+								height={60}
+								className='w-[72px] h-[72px] '
+							/>
+							<Image
+								src={'/typescript.svg'}
+								alt='Logo'
+								width={60}
+								height={60}
+								className='w-[72px] h-[72px] '
+							/>
+						</div>
+						<div className='text-center mt-8'>
+							<span>Fondamentaux</span>
+						</div>
 					</div>
-					<div className='w-1/2'>
-						<span>Fondamentaux</span>
+					<div className='flex-col'>
+						<div className='flex gap-10'>
+							<Image
+								src={'/react-text-logo.png'}
+								alt='Logo'
+								width={1200}
+								height={665}
+								className='w-[140px]'
+							/>
+							<Image
+								src={'/redux-text-logo.png'}
+								alt='Logo'
+								width={1200}
+								height={600}
+								className='w-[150px]'
+							/>
+						</div>
+						<div className='text-center mt-8'>
+							<span>Front-end</span>
+						</div>
 					</div>
-				</div>
-				<div className='flex gap-[200px] justify-between items-center w-[75%] '>
-					<div className='w-[1000px] h-[100px] justify-center gap-8 flex items-center'>
-						<Image
-							src={'/react-text-logo.png'}
-							alt='Logo'
-							width={1200}
-							height={665}
-							className='w-[140px]'
-						/>
-						<Image
-							src={'/redux-text-logo.png'}
-							alt='Logo'
-							width={1200}
-							height={600}
-							className='w-[140px]'
-						/>
-					</div>
-					<div className='w-1/2'>
-						<span>Front end</span>
-					</div>
-				</div>
-				<div className='flex gap-[200px] justify-between items-center w-[75%] mb-[100px] '>
-					<div className='w-[1000px] h-[100px] justify-center flex gap-10 items-center'>
-						<Image
-							src={'/node.svg'}
-							alt='Logo'
-							width={60}
-							height={60}
-							className='w-[100px] h-[100px] '
-						/>
-						<Image
-							src={'/next-text-logo.png'}
-							alt='Logo'
-							width={1200}
-							height={719}
-							className='w-[120px] h-[70px] '
-						/>
-					</div>
-					<div className='w-1/2'>
-						<span>Fullstack</span>
+					<div className='flex-col'>
+						<div className='flex gap-10'>
+							<Image
+								src={'/node.svg'}
+								alt='Logo'
+								width={60}
+								height={60}
+								className='w-[100px] h-[100px] '
+							/>
+							<Image
+								src={'/next-text-logo.png'}
+								alt='Logo'
+								width={1200}
+								height={719}
+								className='w-[120px] h-[70px] translate-y-2'
+							/>
+						</div>
+						<div className='text-center mt-8'>
+							<span>Fullstack</span>
+						</div>
 					</div>
 				</div>
 			</div>
