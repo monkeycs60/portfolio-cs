8d929e8bb2aa2af2a9699a4c90808d7634a07ed9
monkeycs60 <clement.serizay@gmail.com>
2023-09-02 18:04:36 +0200

    style: change tech stack

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,33 +4,28 @@ import React from 'react';
 
 const AboutMe = () => {
 	return (
-		<div className='w-full relative lg:h-[40vh] p-4 flex flex-col bg-secondary text-primary overflow-hidden'>
-			<Image
-				src='/sitted-laptop.png'
-				alt='Logo'
-				width={1020}
-				height={400}
-				priority={true}
-				className={clsx(
-					'absolute right-[400px] -bottom-4 lg:w-[320px] rounded-sm'
-				)}
-			/>
+		<div className='w-full relative pt-[50px] pb-[100px] 3xl:py-0 3xl:h-[40vh] p-4 flex flex-col bg-secondary text-primary overflow-hidden'>
 			<div className='w-[65vw] pt-12 flex-col gap-16 mx-auto'>
-				<h2 className='font-poppins text-3xl uppercase first-letter:text-5xl'>
-					A propos de moi
-				</h2>
+				<div className='flex gap-4'>
+					<h2 className='font-poppins text-3xl'>A propos de moi</h2>{' '}
+					<div className='bg-gray-cool-100 w-[5px] animate-pulse '></div>
+				</div>
 				<div className='mt-[90px] w-[90%] flex flex-col gap-8 font-poppins '>
-					<p className='text-xl w-1/2'>
+					<p className='text-xl w-3/4 3xl:w-1/2'>
 						D'abord formé au développement front-end avec une
 						spécialisation sur le framework React, j'ai continué mon
 						apprentissage en autodidacte afin de pouvoir bâtir des
 						applications fullstack.
 					</p>
-					<p className='text-xl w-1/2'>
-						Depuis, j'ai créé de A à Z <span className='font-bold'> Coworkez Malin</span>, une application communautaire qui permet de référencer et de consulter tous les espaces de coworking gratuits en France
+					<p className='text-xl w-3/4 3xl:w-1/2'>
+						Depuis, j'ai créé de A à Z{' '}
+						<span className='font-bold'> Coworkez Malin</span>, une
+						application communautaire qui permet de référencer et de
+						consulter tous les espaces de coworking gratuits en France
 					</p>
-					<p className='text-xl w-1/2'>
-						Je suis actuellement à la recherche d'un poste de développeur front-end ou fullstack en CDI sur Paris et Bordeaux.
+					<p className='text-xl w-3/4  3xl:w-1/2'>
+						Je suis actuellement à la recherche d'un poste de développeur
+						front-end ou fullstack en CDI sur Paris et Bordeaux.
 					</p>
 				</div>
 			</div>
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -4,72 +4,72 @@ import { MoveDown } from 'lucide-react';
 const TechStack = () => {
 	return (
 		<div className='w-[65vw] mx-auto relative pt-[70px] flex flex-col bg-primary text-secondary overflow-hidden'>
-			<div className='flex flex-col gap-10 justify-center items-center'>
+			<div className='flex flex-col gap-10 justify-center items-center text-2xl'>
 				<div className='flex gap-4 font-poppins  w-full'>
 					<h3 className='text-xl lg:text-3xl '>Tech Stack</h3>
 					<div className='bg-gray-cool-700 w-[5px] animate-pulse '></div>
 				</div>
-				<h3 className='font-poppins text-xl'>Fondamentaux</h3>
-				<div className='flex gap-8'>
-					<div className='w-[400px] h-[140px] bg-yellow-50 border-[2px] flex items-center justify-center'>
+				<div className='flex gap-[200px] justify-between mt-[100px] items-center w-[75%]'>
+					<div className='w-[1000px] h-[100px] justify-center gap-12 flex items-center'>
 						<Image
 							src={'/javascript.svg'}
 							alt='Logo'
 							width={60}
 							height={60}
-							className='w-[60px] h-[60px] '
+							className='w-[72px] h-[72px] '
 						/>
-					</div>
-					<div className='w-[400px] h-[140px] bg-blue-100 border-[2px] flex items-center justify-center'>
 						<Image
 							src={'/typescript.svg'}
 							alt='Logo'
 							width={60}
 							height={60}
-							className='w-[60px] h-[60px] '
+							className='w-[72px] h-[72px] '
 						/>
 					</div>
+					<div className='w-1/2'>
+						<span>Fondamentaux</span>
+					</div>
 				</div>
-				<h3 className='font-poppins text-xl'>Front end</h3>
-				<div className='flex gap-8'>
-					<div className='w-[400px] h-[140px] bg-cyan-50 border-[2px] flex items-center justify-center'>
+				<div className='flex gap-[200px] justify-between items-center w-[75%] '>
+					<div className='w-[1000px] h-[100px] justify-center gap-8 flex items-center'>
 						<Image
 							src={'/react-text-logo.png'}
 							alt='Logo'
 							width={1200}
 							height={665}
-							className='w-[130px]'
+							className='w-[140px]'
 						/>
-					</div>
-					<div className='w-[400px] h-[140px] bg-violet-100 border-[2px] flex items-center justify-center'>
 						<Image
 							src={'/redux-text-logo.png'}
 							alt='Logo'
 							width={1200}
 							height={600}
-							className='w-[130px]'
+							className='w-[140px]'
 						/>
 					</div>
+					<div className='w-1/2'>
+						<span>Front end</span>
+					</div>
 				</div>
-				<h3 className='font-poppins text-xl'>Fullstack</h3>
-				<div className='flex gap-8'>
-					<div className='w-[400px] h-[140px] bg-green-100 border-[2px] flex items-center justify-center'>
+				<div className='flex gap-[200px] justify-between items-center w-[75%] mb-[100px] '>
+					<div className='w-[1000px] h-[100px] justify-center flex gap-10 items-center'>
 						<Image
 							src={'/node.svg'}
 							alt='Logo'
 							width={60}
 							height={60}
-							className='w-[90px] h-[90px] '
+							className='w-[100px] h-[100px] '
 						/>
-					</div>
-					<div className='w-[400px] h-[140px] bg-gray-200 border-[1px]  flex items-center justify-center'>
 						<Image
 							src={'/next-text-logo.png'}
 							alt='Logo'
 							width={1200}
 							height={719}
-							className='w-[95px]'
+							className='w-[120px] h-[70px] '
 						/>
+					</div>
+					<div className='w-1/2'>
+						<span>Fullstack</span>
 					</div>
 				</div>
 			</div>
