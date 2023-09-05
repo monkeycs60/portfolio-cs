dab5e86b1f8bdb06dc1c2ada3891acc002ca7de4
Clement SERIZAY <clement.serizay@gmail.com>
2023-09-05 18:29:22 +0200

    style: finish responsive mobile

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 const Footer = () => {
 	return (
-		<div className='lg:text-sm 2xl:text-base w-screen bg-secondary text-primary py-6 2xl:py-8 flex flex-col items-center font-poppins '>
-			<span className='flex items-center gap-4'>
+		<div className='text-xs lg:text-sm 2xl:text-base w-screen bg-secondary text-primary px-4 py-10 lg:px-0 lg:py-6 2xl:py-8 flex flex-col items-center font-poppins '>
+			<span className='flex flex-wrap justify-center lg:justify-normal lg:flex-nowrap items-center gap-4'>
 				<p>Ce site a été développé en</p>
 				<Image
 					src={'/react.svg'}
 					alt='Logo'
 					width={60}
 					height={60}
-					className='w-[35px] h-[35px]'
+					className='w-[25px] h-[25px] lg:w-[35px] lg:h-[35px]'
 				/>
 				<p>sur le framework</p>
 				<Image
@@ -18,7 +18,7 @@ const Footer = () => {
 					alt='Logo'
 					width={100}
 					height={100}
-					className='w-[70px] h-[70px]'
+					className='w-[50px] h-[50px] lg:w-[70px] lg:h-[70px]'
 				/>
 				<p>Powered by </p>
 				<Image
@@ -26,7 +26,7 @@ const Footer = () => {
 					alt='Logo'
 					width={1539}
 					height={387}
-					className='w-[100px] h-[30px]'
+					className='w-[70px] h-[20px] lg:w-[100px] lg:h-[30px]'
 				/>
 			</span>
 			<div className='flex  items-center'></div>
